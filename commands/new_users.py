0bfec43458bde49d266a5d7c384057971e235dbf
vlade11115 <vlade11115@gmail.com>
2019-12-20 11:53:33 +0200

    Fix broken permissions for newcomers.

--- commands/new_users.py
+++ commands/new_users.py
@@ -53,6 +53,9 @@ def restrict(message):
             chat_id=config.chat_id,
             user_id=new_member.id,
             can_send_other_messages=False,
+            can_send_messages=True,
+            can_send_media_messages=True,
+            can_add_web_page_previews=True,
         )
 
     session.close()
