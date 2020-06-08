f2eb50bad0f809340c18fe960eb0a498c0aa2f9b
Konstantin Zemlyak <zart@zartsoft.ru>
2020-06-08 20:51:15 +0500

    Initialize logging

--- src/rpb_bot/main.py
+++ src/rpb_bot/main.py
@@ -9,14 +9,33 @@ if "SENTRY_SDK" in os.environ:
     except ImportError:
         pass
 
+import json
+from logging import basicConfig, getLogger
+from logging.config import fileConfig, dictConfig
 from .config import get_config
+
+log = getLogger(__name__)
 
 
 def run():
+    # configuration
     config = get_config()
-    print(config)
 
     # init logging
+    log_path = config["log_config"] or config["config"]
+    try:
+        if log_path:
+            if log_path.endswith(".json"):
+                with open(log_path) as fp:
+                    dictConfig(json.load(fp))
+                log.debug("Configured logging from JSON: %s", log_path)
+            else:
+                fileConfig(log_path)
+                log.debug("Configured logging from INI: %s", log_path)
+    except Exception:
+        basicConfig("%(loglevel)-5s [%(name)s] %(message)s")
+        log.debug("Falling back on basic logging config", exc_info=True)
+
     # instantiate the bot
     # check connection to database and redis
     # start the bot if everything is fine
