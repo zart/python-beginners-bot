4e2a831e198ebfb9a8eb5ecf21afa730d3e7a294
Konstantin Zemlyak <zart@zartsoft.ru>
2020-06-08 20:46:57 +0500

    Sentry SDK support

--- src/rpb_bot/main.py
+++ src/rpb_bot/main.py
@@ -1,7 +1,16 @@
 "Main entry"
-# TODO: init sentry as early as possible
+
+import os
+
+# init sentry as early as possible, but only when requested with env var
+if "SENTRY_SDK" in os.environ:
+    try:
+        __import__("sentry_sdk").init(os.getenv("SENTRY_SDK"))
+    except ImportError:
+        pass
 
 from .config import get_config
+
 
 def run():
     config = get_config()
